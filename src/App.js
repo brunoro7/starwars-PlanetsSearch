a617418a109314512d60dcbe8173ea8efaee0009
brunoro7 <bruno.ro7c@gmail.com>
2022-04-18 22:42:21 -0300

    tentando consertar erro do lint.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import StarWarsProvider from './context/StarWarsProvider';
 import './index.css';
 import Home from './page/Home';
@@ -7,11 +7,9 @@ import Home from './page/Home';
 function App() {
   return (
     <StarWarsProvider>
-      <BrowserRouter>
-        <Switch>
-          <Route to="/" component={ Home } />
-        </Switch>
-      </BrowserRouter>
+      <Switch>
+        <Route to="/" component={ Home } />
+      </Switch>
     </StarWarsProvider>
   );
 }
